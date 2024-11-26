3325cd274f56c328aa22f13fa832c6528cdeeab1
ThePrimeagen <the.primeagen@aol.com>
2024-11-26 20:52:37 +0200

    A: Base files

---
+++ main.py
@@ -0,0 +1,24 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        
+        screen.fill("black")
+        pygame.display.flip()
+    
+
+
+if __name__ == "__main__":
+    main()
+
